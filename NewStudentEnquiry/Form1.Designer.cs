872a130a67593fe5e1786a1193870d1b2ae08c68
Owner <ij5775wy@go.minnstate.edu>
2020-04-21 21:41:15 -0500

    Add project files.

---
+++ NewStudentEnquiry/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace NewStudentEnquiry
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblDegreeCount = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbxDepartment = new System.Windows.Forms.ComboBox();
+            this.lstDegrees = new System.Windows.Forms.ListBox();
+            this.cbxHowDidYouHear = new System.Windows.Forms.ComboBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Which department are you interested in?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(219, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "What degree programs are you interested in?";
+            // 
+            // lblDegreeCount
+            // 
+            this.lblDegreeCount.AutoSize = true;
+            this.lblDegreeCount.Location = new System.Drawing.Point(237, 199);
+            this.lblDegreeCount.Name = "lblDegreeCount";
+            this.lblDegreeCount.Size = new System.Drawing.Size(56, 13);
+            this.lblDegreeCount.TabIndex = 2;
+            this.lblDegreeCount.Text = "0 selected";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(140, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "How did you hear about us?";
+            // 
+            // cbxDepartment
+            // 
+            this.cbxDepartment.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxDepartment.FormattingEnabled = true;
+            this.cbxDepartment.Location = new System.Drawing.Point(26, 25);
+            this.cbxDepartment.Name = "cbxDepartment";
+            this.cbxDepartment.Size = new System.Drawing.Size(278, 21);
+            this.cbxDepartment.TabIndex = 4;
+            this.cbxDepartment.SelectedIndexChanged += new System.EventHandler(this.cbxDepartment_SelectedIndexChanged);
+            // 
+            // lstDegrees
+            // 
+            this.lstDegrees.FormattingEnabled = true;
+            this.lstDegrees.Location = new System.Drawing.Point(26, 101);
+            this.lstDegrees.Name = "lstDegrees";
+            this.lstDegrees.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.lstDegrees.Size = new System.Drawing.Size(278, 95);
+            this.lstDegrees.Sorted = true;
+            this.lstDegrees.TabIndex = 5;
+            this.lstDegrees.SelectedIndexChanged += new System.EventHandler(this.lstDegrees_SelectedIndexChanged);
+            // 
+            // cbxHowDidYouHear
+            // 
+            this.cbxHowDidYouHear.FormattingEnabled = true;
+            this.cbxHowDidYouHear.Location = new System.Drawing.Point(26, 239);
+            this.cbxHowDidYouHear.Name = "cbxHowDidYouHear";
+            this.cbxHowDidYouHear.Size = new System.Drawing.Size(278, 21);
+            this.cbxHowDidYouHear.TabIndex = 6;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(128, 295);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(73, 23);
+            this.btnSubmit.TabIndex = 7;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnSubmit;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 345);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.cbxHowDidYouHear);
+            this.Controls.Add(this.lstDegrees);
+            this.Controls.Add(this.cbxDepartment);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblDegreeCount);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Student Enquiry";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblDegreeCount;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbxDepartment;
+        private System.Windows.Forms.ListBox lstDegrees;
+        private System.Windows.Forms.ComboBox cbxHowDidYouHear;
+        private System.Windows.Forms.Button btnSubmit;
+    }
+}
+
---
+++ NewStudentEnquiry/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NewStudentEnquiry
+{
+    public partial class Form1 : Form
+    {
+
+        SortedList<String, String[]> programs;
+        string[] ITPrograms = {"Programming", "Security", "Networking"};
+        string[] EngineeringPrograms = {"Electrical", "Mechanical"};
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            programs = new SortedList<string, string[]>
+            {
+                {"Information Technology", ITPrograms},
+                {"Engineering", EngineeringPrograms}
+            };
+
+            cbxDepartment.Items.AddRange(programs.Keys.ToArray());
+            cbxDepartment.SelectedIndex = 0;
+
+            cbxHowDidYouHear.Items.Add("Another student");
+            cbxHowDidYouHear.Items.Add("Another school");
+            cbxHowDidYouHear.Items.Add("Internet search");
+        }
+
+        private void cbxDepartment_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            /* Is there a way to directly invoke the lstDegrees_SelectedIndexChanged method,
+             * or is this the only way to cause it to activate? Because what if what's being
+             * designed is more complicated than clearing one or two lists? */
+            lstDegrees.ClearSelected();
+
+            lstDegrees.Items.Clear();
+
+            string department = cbxDepartment.Text;
+
+            if (department != null)
+            {
+                string[] degrees = programs[department];
+                lstDegrees.Items.AddRange(degrees);
+            }
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            List<string> errors = new List<string>();
+
+            /* Is this if statement necessary if a selected index is forced when the form loads?
+             * If it is, then the error should tell the user to select a department or field,
+             * but the slides have us tell the user to select a semester instead */
+            if (cbxDepartment.SelectedIndex == -1)
+            {
+                errors.Add("Please select a department");
+            }
+
+            if (lstDegrees.SelectedIndex == -1)
+            {
+                errors.Add("Please select at least one degree");
+            }
+
+            if (String.IsNullOrEmpty(cbxHowDidYouHear.Text))
+            {
+                errors.Add("Please type or select how you heard about us");
+            }
+
+            if (errors.Count > 0)
+            {
+                MessageBox.Show(String.Join("\n", errors), "Error");
+                return;
+            }
+
+            StringBuilder summaryBuilder = new StringBuilder();
+
+            summaryBuilder.Append("Summary");
+            summaryBuilder.Append("\n\nDepartment: ");
+            summaryBuilder.Append(cbxDepartment.Text);
+
+            summaryBuilder.Append("\n\nPrograms: ");
+            summaryBuilder.Append(lstDegrees.SelectedItems.Count);
+            summaryBuilder.Append(" selected\n");
+
+            foreach (object degree in lstDegrees.SelectedItems)
+            {
+                summaryBuilder.Append(degree);
+                summaryBuilder.Append("\n");
+            }
+
+            summaryBuilder.Append("\nYou heard about us from: ");
+            summaryBuilder.Append(cbxHowDidYouHear.Text);
+
+            string summary = summaryBuilder.ToString();
+
+            MessageBox.Show(summary, "Thank you!");
+
+            this.Close();
+        }
+
+        private void lstDegrees_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int numberSelected = lstDegrees.SelectedItems.Count;
+            lblDegreeCount.Text = $"{numberSelected} selected";
+        }
+    }
+}
